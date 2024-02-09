eb543aec154ecff73632a0c7f16f6c1226b04b76
sumaya <sumaya.alqadhy@gmail.com>
2024-02-08 16:28:28 -0800

    added app file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 if (process.env.USER) require("dotenv").config();
-
+const cors = require("cors");
 const express = require("express");
-const app = express();
+const notFound = require("./errors/notFound");
 const moviesRouter = require("./movies/movies.router");
-const theatersRouter = require("./theaters/theaters.router");
-const reviewsRouter = require("./reviews/reviews.router");
+const theaterRouter = require("./theaters/theaters.router");
+const errorHandler = require("./errors/errorHandler");
+const reveiwRouter = require("./reviews/reviews.router");
 
-// TODO: Add your code here
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/movies", moviesRouter);
+app.use("/reviews", reveiwRouter);
+app.use("/theaters", theaterRouter);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 module.exports = app;
